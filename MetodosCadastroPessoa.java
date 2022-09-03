2d88b877d0a0b5c71b7b7d5aae672b5911e0f60c
Vitor <vitindossm@gmail.com>
2022-09-03 09:32:00 -0300

    Criando metodo informacoes

---
+++ MetodosCadastroPessoa.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+public class MetodosCadastroPessoa {
+    Pessoa pessoa = new Pessoa();
+    Map <String, Pessoa> pessoas = new HashMap<>();
+
+    public boolean adicionaPessoa(String cpf, Pessoa pessoa){
+        try{
+            pessoas.put(cpf,pessoa);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    public boolean removePessoa(String cpf){
+        try{
+            pessoas.remove(pessoa);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    public boolean informacoes(){
+     return false;
+    }
+}
