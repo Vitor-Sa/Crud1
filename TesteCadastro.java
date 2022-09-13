8ab3813bdd10dcb86273ff40ccc042c6b2c4b631
Vitor <vitindossm@gmail.com>
2022-09-13 09:38:42 -0300

    Adicionando pessoa

--- TesteCadastro.java
+++ TesteCadastro.java
@@ -1,25 +1,57 @@
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.Scanner;
 
 
 public class TesteCadastro {
-    static MetodosCadastroPessoa mcp = new MetodosCadastroPessoa();
+
     public static void main(String[] args) {
-        popularPessoa();
-        verPessoa();
-    }
-    public static void popularPessoa(){
-        Pessoa pessoa1 = new Pessoa("Carl",19,"999","0001");
-        mcp.adicionaPessoa("0001",pessoa1);
-        Pessoa pessoa2 = new Pessoa("Carlo",23,"899","0002");
-        mcp.adicionaPessoa("0002",pessoa2);
-        System.out.println("-------------------------\n");
+        MetodosCadastroPessoa mcp = new MetodosCadastroPessoa();
+        Scanner scan = new Scanner(System.in);
+        mostrarMenu();
 
+        int comando = scan.nextInt();
+        while (comando != 5) {
+
+            switch (comando) {
+                case 1:
+                    Pessoa pessoa = criandoPessoa(scan);
+                    mcp.adicionaPessoa(pessoa.getCpf(), pessoa);
+                    break;
+            }
+            mostrarMenu();
+            comando = scan.nextInt();
+        }
     }
 
-    public static void verPessoa(){
-        Pessoa pessoa2 = new Pessoa("Karlos",40,"799","0002");
-        mcp.informacoes("0002");
+    public static void mostrarMenu() {
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("Digite os numeros para as seguintes operacoes");
+        System.out.println("1- Cadastrar pessoa");
+        System.out.println("2- Atualizar informacoes");
+        System.out.println("3- ver informacoes");
+        System.out.println("4- Deletar pessoa");
+        System.out.println("5- Encerrar programa");
+        System.out.println("-------------------------------------------------");
+    }
+
+    public static Pessoa criandoPessoa(Scanner scan) {
+
+        System.out.println("Digite o nome");
+        String nome = scan.next();
+        System.out.println("Digite a idade");
+        int idade = scan.nextInt();
+        System.out.println("Digite Cpf");
+        String cpf = scan.next();
+        System.out.println("Digite E-nail");
+        String email = scan.next();
+        System.out.println("Digite telefone");
+        String telefone = scan.next();
+
+        Pessoa pessoa = new Pessoa(nome, idade, telefone, cpf, email);
+
+        return pessoa;
+
     }
 
 
